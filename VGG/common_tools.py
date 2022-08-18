f66cf3fe8a76b1c3e91cdaf5e5c67a24124b8e47
ceazer <ceazerdl@gmail.com>
2022-08-18 12:09:34 +0800

    更新get_vgg16代码

---
+++ VGG/common_tools.py
@@ -0,0 +1,23 @@
+import torch
+import torchvision.models as models
+
+
+def get_vgg16(path_state_dict, device, vis_model=False):
+    """
+    创建模型，加载参数
+    :param path_state_dict:
+    :param device:
+    :param vis_model:
+    :return:
+    """
+    model = models.vgg16()
+    pretrained_state_dict = torch.load(path_state_dict)
+    model.load_state_dict(pretrained_state_dict)
+    model.eval()
+
+    if vis_model:
+        from torchsummary import summary
+        summary(model, input_size=(3, 224, 224), device="cpu")
+
+    model.to(device)
+    return model
