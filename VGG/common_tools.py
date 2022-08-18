e388689a6e659de987118de524e30428752135cd
ceazer <ceazerdl@gmail.com>
2022-08-19 01:35:27 +0800

    修改get_vgg16.py，将输出类别改为2

--- VGG/common_tools.py
+++ VGG/common_tools.py
@@ -1,5 +1,6 @@
 import torch
 import torchvision.models as models
+import torch.nn as nn
 
 
 def get_vgg16(path_state_dict, device, vis_model=False):
@@ -13,6 +14,11 @@ def get_vgg16(path_state_dict, device, vis_model=False):
     model = models.vgg16()
     pretrained_state_dict = torch.load(path_state_dict)
     model.load_state_dict(pretrained_state_dict)
+
+    # 本数据集为2分类，所以更改vgg模型的输出为2
+    num_features = model.classifier[6].in_features
+    new_linear = nn.Linear(num_features, 2)
+    model.classifier._modules["6"] = new_linear
 
     if vis_model:
         from torchsummary import summary
